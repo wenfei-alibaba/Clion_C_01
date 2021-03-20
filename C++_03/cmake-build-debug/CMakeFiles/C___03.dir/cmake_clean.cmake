c065bea640332e89dd84f9c186f660e928eb94e3
wenfei-alibaba <xuwenfei2020@aliyun.com>
2021-03-21 03:10:05 +0800

    Clion第一个项目

---
+++ C++_03/cmake-build-debug/CMakeFiles/C___03.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C___03.dir/main.c.obj"
+  "C___03.exe"
+  "C___03.exe.manifest"
+  "C___03.pdb"
+  "libC___03.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C___03.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
